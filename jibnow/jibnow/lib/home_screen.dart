1a93d915c8ba850e48566e44b214b02f642ef412
yasofl <mineking2019@gmail.com>
2025-08-29 11:47:42 +0100

    Add files via upload

---
+++ jibnow/jibnow/lib/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Jib Now - Home'),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to Jib Now!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
